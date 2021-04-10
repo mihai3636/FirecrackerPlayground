66e168101a6c30859df4c367c77bee5fef02e64e
mihaidogaru2537 <dogarumihai2537@gmail.com>
2021-04-10 19:26:38 +0300

    Fixed the wrongfully usage of modules. Now it takes too long to build the whole firecracker.

--- firecracker/src/dpdk_component/src/dpdk_component.rs
+++ firecracker/src/dpdk_component/src/dpdk_component.rs
@@ -1,14 +1,9 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#[allow(dead_code)]
-mod bindingsMbuf;
 
-use bindingsMbuf::{
+use crate::bindingsMbuf::{
     rte_eal_init, rte_eal_process_type, rte_mbuf, rte_mempool_lookup,
     rte_proc_type_t_RTE_PROC_PRIMARY, rte_ring_dequeue_real, rte_ring_lookup,
 };
 
-pub fn play_winning_sound(name: String) {
-    println!("Playing winning sound {}", name);
+pub fn test_func() {
+    println!("Hellooo, is this working?");
 }
--- firecracker/src/dpdk_component/src/lib.rs
+++ firecracker/src/dpdk_component/src/lib.rs
@@ -1,3 +1,32 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#[allow(dead_code)]
+mod bindingsMbuf;
+
+mod dpdk_component;
+
+
+// use std::ffi::CString;
+// use std::os::raw::c_void;
+// use std::ptr::null_mut;
+
+// use std::process;
+// use std::{thread, time};
+
+
+
+// use bindingsMbuf::{
+//     rte_eal_init, rte_eal_process_type, rte_mbuf, rte_mempool_lookup,
+//     rte_proc_type_t_RTE_PROC_PRIMARY, rte_ring_dequeue_real, rte_ring_lookup,
+// };
+
+pub fn play_winning_sound(name: String) {
+    println!("Playing winning sound {}", name);
+    dpdk_component::test_func();
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -5,15 +34,3 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
-
-
-
-use std::ffi::CString;
-use std::os::raw::c_void;
-use std::ptr::null_mut;
-
-use std::process;
-use std::{thread, time};
-
-pub mod dpdk_component;
-
