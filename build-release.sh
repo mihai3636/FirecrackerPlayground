68573bc4e6f9d2b0ecf667a9bf38629e7f9ee967
mihaidogaru2537 <dogarumihai2537@gmail.com>
2021-05-28 15:00:40 +0300

    Build and run RELEASE scripts, receive deq working but needs improvement

---
+++ build-release.sh
@@ -0,0 +1,2 @@
+cd ./firecracker
+RUSTFLAGS="-lrte_eal -lrte_mempool -lrte_mbuf -lrte_ring -C target-feature=-crt-static" cargo build --target=x86_64-unknown-linux-gnu --release
---
+++ run-release.sh
@@ -0,0 +1,2 @@
+rm /tmp/firecracker.socket
+./firecracker/build/cargo_target/x86_64-unknown-linux-gnu/release/firecracker --api-sock /tmp/firecracker.socket
